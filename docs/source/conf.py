7190799c4d4ecd76643dca4d8ae51e1194cf8799
ggenelot <77398544+ggenelot@users.noreply.github.com>
2025-11-20 13:35:25 +0100

    Minor changes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,10 @@ extensions = [
 templates_path = ['_templates']
 exclude_patterns = []
 
-
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
